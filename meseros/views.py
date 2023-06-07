2765afb6674fc3f6f631cdf7e6e581ce283aa1ea
Eduardo Haro <eduardoharovillanueva@gmail.com>
2023-06-06 20:46:44 -0500

    Commit 2 para la segunda práctica

--- meseros/views.py
+++ meseros/views.py
@@ -6,7 +6,7 @@ from meseros.models import Meseros
 def meseros_list(request):
     query = Q(procedencia='Perú') & Q(edad__lte=30)
     # aumentar la edad 5 años a todos los meseros
-    # Meseros.objects.update(edad=F('edad')-5)
+    # Meseros.objects.update(edad=F('edad')+5)
     data_context = Meseros.objects.filter(query)
     return render(request, 'meseros/meseros_list.html', context={'data': data_context})
 
